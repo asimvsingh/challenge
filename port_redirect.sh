4ba84da1b6b73702d5f27c60b3e49b34b95ac725
Asim Singh <avsingh@us.ibm.com>
2015-11-02 02:01:18 -0600

    commiting port_redirect with contrack

--- port_redirect.sh
+++ port_redirect.sh
@@ -7,8 +7,8 @@ do
   echo "executing: sudo iptables -t nat  -D PREROUTING $i"
   sudo iptables -t nat  -D PREROUTING $i
 done
-
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $constant_port -m state --state NEW -j REDIRECT --to-port $app_port  || (echo "ERROR: redirect $constant_port to $app_port failed" ; exit 1)
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $constant_port -m conntrack --ctstate NEW -j REDIRECT --to-port $app_port  || (echo "ERROR: redirect $constant_port to $app_port failed" ; exit 1)
+#sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $constant_port -m state --state NEW -j REDIRECT --to-port $app_port  || (echo "ERROR: redirect $constant_port to $app_port failed" ; exit 1)
 #open the traffic on the $app_port
 sudo iptables -A INPUT -p tcp -m tcp --dport $app_port -j ACCEPT
 echo "Dump the new redirect rules"
